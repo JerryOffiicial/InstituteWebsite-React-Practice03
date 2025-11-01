ea86449850c13c0b998a928595025b9ef1733fcd
JerryOffiicial <jerrysonjerry1234@email.com>
2025-11-01 21:12:49 +0530

    Components Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
 
 function App() {
   return (
     <div>
-      App
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+         <Programs/>
+      </div>
+      
     </div>
   )
 }
